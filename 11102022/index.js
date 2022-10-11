d0ab25d08916f9a4aeae0ed6d64dfa63936af6a5
Shivproj <shivindrakanti@gmail.com>
2022-10-11 12:20:55 +0530

    isNaN() + nested if else

--- 11102022/index.js
+++ 11102022/index.js
@@ -1,9 +1,12 @@
 //using event listener
 const ip1 = document.querySelector("input");
-ip1.addEventListener("input", check);
+ip1.addEventListener("change", check);
 
 function check() {
   const n = ip1.value;
+  if(isNaN(n))
+  {alert("Enter a number")}
+  else{
   if (n % 2 == 0) document.querySelector("#result").value = "Even";
   else document.querySelector("#result").value = "Odd";
-}
+}}
