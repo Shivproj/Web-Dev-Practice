9e5c3e908d2fbab0b01631488e746eb9d8f43f18
Shivproj <shivindrakanti@gmail.com>
2022-10-12 13:12:54 +0530

    tried remove child but failed

--- 12102022/index.js
+++ 12102022/index.js
@@ -1,33 +1,28 @@
 //Radio button logic (only works with button)
 
-document.querySelector("#math").addEventListener("change",check)
-document.querySelector("#physics").addEventListener("change",check)
-document.querySelector("#chemistry").addEventListener("change",check)
+document.querySelector("#math").addEventListener("change", check);
+document.querySelector("#physics").addEventListener("change", check);
+document.querySelector("#chemistry").addEventListener("change", check);
 
 function check() {
-  if (document.querySelector("#math").checked)  console.log("Maths")
-  else if (document.querySelector("#physics").checked) console.log("Physics")
-  else console.log("Chemistry")
+  if (document.querySelector("#math").checked) console.log("Maths");
+  else if (document.querySelector("#physics").checked) console.log("Physics");
+  else console.log("Chemistry");
 }
 
 //Grade Calculator
-let box1 = document.querySelector("#box1");// <let> This works because function is being "called" outside in line 13
-
+let box1 = document.querySelector("#box1"); // <let> This works because function is being "called" outside in line 13
 
 box1.addEventListener("change", calc);
 counter = 0;
 
-
 function calc() {
-    let score = box1.value;
- 
-  
+  let score = box1.value;
+
   let box2 = document.createElement("input");
   box2.id = "Final";
   const label = document.createElement("label");
-  label.innerText = "Your grade is : "
-
-  
+  label.innerText = "Your grade is : ";
 
   if (counter == 0) {
     add.appendChild(label);
@@ -38,36 +33,26 @@ function calc() {
     } else {
       if (score >= 80) {
         document.querySelector("#Final").value = "A";
-       
       } else if (60 <= score && score < 80) {
         document.querySelector("#Final").value = "B";
-        
       } else if (35 <= score && score < 60) {
         document.querySelector("#Final").value = "C";
-        
       } else {
         document.querySelector("#Final").value = "D";
-        
       }
     }
-  } 
-  else {    
-
+  } else {
     if (score > 99 || score < 0) {
       alert("Please enter a valid number");
     } else {
       if (score >= 80) {
         document.querySelector("#Final").value = "A";
-        
       } else if (60 <= score && score < 80) {
         document.querySelector("#Final").value = "B";
-        
       } else if (35 <= score && score < 60) {
         document.querySelector("#Final").value = "C";
-        
       } else {
         document.querySelector("#Final").value = "D";
-       
       }
     }
   }
@@ -76,42 +61,29 @@ function calc() {
 
 //Form validation with change of input fields
 
-
-
 let fname = document.querySelector("#fname");
-fname.addEventListener("blur",validate1)
+fname.addEventListener("blur", validate1);
+
+let lname = document.querySelector("#lname");
+lname.addEventListener("blur", validate2);
 
 function validate1() {
-  let fname  = document.querySelector("#fname").value 
-  
-  if(fname.length == 0) {
-    
-    
-    x.innerText = "Please enter a name"
-    add2.appendChild(x)
-    x.style.color="red";
-
+  let fname = document.querySelector("#fname").value;
+  if (fname.length == 0) {
+    let x = document.createElement("p");
+    x.innerText = "Please enter a First Name";
+    add2.appendChild(x);
+    x.style.color = "red";
   }
-  else{
-    add2.removeChild(x);
-  }
-
 }
-let lname = document.querySelector("#lname");
-lname.addEventListener("blur",validate2)
 
 function validate2() {
- 
-  let lname  = document.querySelector("#lname").value 
-  if(lname.length == 0) {
-    
-    let x = document.createElement("p") 
-    x.innerText = "Please enter a name"
-    add3.appendChild(x)
-    x.style.color="red";
+  let lname = document.querySelector("#lname").value;
+  if (lname.length == 0) {
+    let y = document.createElement("p");
 
-
+    y.innerText = "Please enter a name";
+    add3.appendChild(y);
+    y.style.color = "red";
   }
-
 }
-
