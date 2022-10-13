17b697950ca463918936dbf04792f611dbc19742
Shivproj <shivindrakanti@gmail.com>
2022-10-13 10:15:46 +0530

    Some Changes to folder strucutre

---
+++ test/functions.js
@@ -0,0 +1,56 @@
+// //FACTIORIAL
+// let x = prompt("Enter a number")
+
+// function fact(x){
+// if(x==0) return 1;
+// if(x==1) return 1;
+
+// return x*(fact(x-1));
+
+
+// }
+
+
+
+
+
+
+
+// //ADDITION
+// console.log(fact(x));
+
+// let m = window.prompt("Enter number 1")
+// let n = prompt("Enter number 2")
+// let p = prompt("Enter number 3")
+
+// function add(m,n,p){
+
+// console.log(parseFloat(m)+parseFloat(n)+parseFloat(p))};
+// add(m,n,p);
+
+// //EVEN OR ODD
+
+// let e = prompt("Enter to check if even or odd")
+// function  check(n){
+// if(n%2==0) console.log("even");
+// else console.log("odd");
+// }
+// check(parseFloat(e));
+
+// //SUBTRACTION
+
+// let a = prompt("enter the first number");
+// let b = prompt("enter the second number");
+// function sub(a,b){
+// console.log(parseFloat(a)-parseFloat(b));
+// }
+// sub(a,b);
+
+
+
+
+
+
+    
+document.getElementById(p).innerHTML+=  "Shiv Sai"
+document.getElementById(p).innerHTML+=  "Shiv Sai"
