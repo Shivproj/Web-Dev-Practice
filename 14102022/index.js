8be234c411169781e3317a263870b8a0653ebc17
Shivproj <shivindrakanti@gmail.com>
2022-10-14 11:52:41 +0530

    Find for each and filter on input

---
+++ 14102022/index.js
@@ -0,0 +1,44 @@
+arr = [];
+let btn1 = document.querySelector("#submit");
+btn1.addEventListener("click", submit);
+let btn2 = document.querySelector("#display");
+btn2.addEventListener("click", display);
+let btn3 = document.querySelector("#find");
+btn3.addEventListener("click", finds);
+let btn4 = document.querySelector("#filter");
+btn4.addEventListener("click", filters);
+let btn5 = document.querySelector("#forEach");
+btn5.addEventListener("click", forEachs);
+
+function submit() {
+  let obj = {};
+  obj.RollNo = document.querySelector("#roll").value;
+  obj.Name = document.querySelector("#name").value;
+  obj.Age = document.querySelector("#age").value;
+  obj.Email = document.querySelector("#email").value;
+  obj.Marks = document.querySelector("#marks").value;
+
+  arr.push(obj);
+}
+
+function display() {
+  console.log(arr);
+}
+function finds() {
+  let found = arr.find(function (item) {
+    return item.Marks > 80;
+  });
+  console.log(found);
+}
+function filters() {
+  let filtered = arr.filter(function (item) {
+    return item.Marks > 70;
+  });
+  console.log(filtered);
+}
+function forEachs() {
+    
+  arr.forEach(function (item) {
+    if (item.Age > 19) console.log(item);
+  });
+}
