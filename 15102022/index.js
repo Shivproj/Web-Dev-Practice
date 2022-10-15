e1bf022b9a64e017a20c19d085bb4465fd195eb0
Shivproj <shivindrakanti@gmail.com>
2022-10-15 16:24:00 +0530

    Prime number

---
+++ 15102022/index.js
@@ -0,0 +1,27 @@
+const x = document.querySelector("#input");
+x.addEventListener("input",input );
+
+
+
+function input(){
+   let n =  x.value;
+   document.querySelector("#result").value = check(n);
+
+
+}
+
+function check(n){
+    isPrime = true;
+    if(n<=1) return "Not Possible";    
+    else{
+    for(let i =2;i<=n/2;i++){
+        if(n%i==0) {                
+        isPrime = false;
+        break;}
+    
+}
+if(isPrime==true) return "Prime";
+else return "Composite";
+}
+
+}
