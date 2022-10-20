04e52b8a636040f6ed5f3640588abb054936c6fe
Shivproj <shivindrakanti@gmail.com>
2022-10-20 17:36:41 +0530

    Search Functionality Added

--- 20102022/CRUD practice/src/student.js
+++ 20102022/CRUD practice/src/student.js
@@ -1,93 +1,56 @@
-document.querySelector("#create").addEventListener("click", create);
-function create() {
-    function validName(name) {
-      isValid = true;
-      message = "";
-      if (!name) {
-        isValid = false;
-        message = "isRequired";
-      }
-      else if (!isNaN(name)) {
-        isValid = false;
-        message = "Name cant include numbers";
-      }
-      return { isValid, message };
-    }
-    function validEyes(eyes) {
-      isValid = true;
-      message = "";
-      if (!eyes) {
-        isValid = false;
-        message = "isRequired";
-      }
-  
-      return { isValid, message };
-    }
-    let formCreate = `<form name="create">
+let data = document.querySelector("#search")
+data.addEventListener("change",search);
+
+
+function search(){
+    let info = data.value;
     
+    x=[];
+    if(!isNaN(info)){
+ 
+
+    students.forEach(function(student){
+        if(student.Gender==info) {
+            x.push(student);
+            return student;
+        }
+    })
+}
+
+else if(isNaN(info)){
+    students.forEach(function(student){
+        if(student.Eyes==info) {
+            x.push(student);
+            return student;
+        }
+    })
     
-  
-    <div class="mb-3 form-check">
-    <label>Name</label>
-    <input type="text" name="Name" >
-    <span hidden id="Name">is Required</span>
-      
-    </div>
-    <div class="mb-3 form-check">
-    <label>Gender</label>
-    <input type="checkbox" name="Gender" >
-    <span hidden id="Gender">is Required</span>
-      
-    </div>
-    <div class="mb-3 form-check">
-    <label>Eyes</label>
-    <input type="text" name="Eyes" >
-    <span hidden id="Eyes">is Required</span>
-      
-    </div>
-    <button type="button" id="Submit" class="btn btn-primary">Submit</button>
-  </form>`;
-  
-    document.querySelector("#content").innerHTML = formCreate;
-    document.querySelector("#Submit").addEventListener("click", submit);
-    function submit() {
-      const form = document.forms["create"];
-  
-      const Name = form.Name.value;
-  
-      if (!form.Gender.checked) Gender = 0;
-      else Gender = 1;
-  
-      const Eyes = form.Eyes.value;
-      let obj = {
-        ID: students.length + 1,
-        Name: Name,
-        Gender: Gender,
-        Eyes: Eyes,
-      };
-  
-      //Validations
-      let checkName = validName(Name);
-      let checkEyes = validEyes(Eyes);
-      if (!checkName.isValid) {
-        document.querySelector("#Name").hidden = false;
-        document.querySelector("#Name").innerText = checkName.message;
-  
-        return;
-      } else {
-        document.querySelector("#Name").hidden = true;
+}
+
+
+
+    function pushStudents() {
+        x.forEach(pushStudent);
       }
-      if (!checkEyes.isValid) {
-        document.querySelector("#Eyes").hidden = false;
-        document.querySelector("#Eyes").innerText = checkEyes.message;
-  
-        return;
-      } else {
-        document.querySelector("#Eyes").hidden = true;
+      function pushStudent(student) {
+        let template = `<tr><td>${student.ID}</td><td>${student.Name}</td><td>${student.Gender}</td><td>${student.Eyes}</td></tr>`;
+    
+        document.querySelector("#table_body").innerHTML += template;
       }
-  
-      students.push(obj);
-      display();
+      document.querySelector("#content").innerHTML = `<table border="1px" id="main_table">
+    <thead><th>ID</th>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Eyes</th>
+    </thead>
+    <tbody id="table_body"></tbody>
+    </table>`;
+    
+      pushStudents();
     }
-  }
-  
+
+
+
+
+
+
