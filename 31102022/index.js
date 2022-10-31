31053c5724969ed92535f62eaa8e76dbbdb5ebfb
Shivproj <shivindrakanti@gmail.com>
2022-10-31 17:50:21 +0530

    reference and value

--- 31102022/index.js
+++ 31102022/index.js
@@ -1,9 +1,29 @@
-try{
-    throw "Invalid";
-}
-catch(error){
-    console.log(error);
-}
+// try{
+//     let a=b;
+// }
+
+// catch(error){
+//    if(error instanceof ReferenceError){
+//     console.log("Not defined");
+//    }
+// }
 
 
-throw("Invalid")
+// for(i =0;i<10;i++)
+// {   var x =5;
+
+//     console.log(i);
+// }
+
+// console.log(i+5);
+// console.log(x);
+
+
+
+let obj={a:10,b:20};
+// _obj={a:100,b:200};
+
+ let _obj = obj
+ _obj.a = 100;
+
+console.log(obj.a);
