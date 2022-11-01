cce5c71b5307dc50dea9fea8b55c64efe2f03bd3
Shivproj <shivindrakanti@gmail.com>
2022-11-01 15:10:37 +0530

    date methods

---
+++ 01102022/index.js
@@ -0,0 +1,24 @@
+let dt = new Date();
+console.log(dt);
+console.log(dt.getMonth());
+console.log(dt.getFullYear());
+console.log(dt.getDate())
+console.log(dt.getDay());
+console.log(dt.getHours());
+console.log(dt.getMinutes());
+console.log(dt.getSeconds());
+console.log(dt.getMilliseconds());
+console.log(dt.getTime());
+dt.setHours(7);
+console.log(dt.getHours());
+
+console.log(Math.PI);
+
+Array=[8,6,7,9,8];
+console.log(Math.max(9,1));
+console.log(Math.round(9.48));
+console.log(Math.sqrt(9));
+console.log(Math.ceil(2.4));
+console.log(Math.floor(2.4));
+console.log(Math.abs(-232))
+
