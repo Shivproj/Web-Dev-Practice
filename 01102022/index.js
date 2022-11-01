c18f9d62ab7de5896dfed3ee179db38eb5a151fd
Shivproj <shivindrakanti@gmail.com>
2022-11-01 21:38:03 +0530

    css circle

--- 01102022/index.js
+++ 01102022/index.js
@@ -2,7 +2,7 @@ let dt = new Date();
 console.log(dt);
 console.log(dt.getMonth());
 console.log(dt.getFullYear());
-console.log(dt.getDate())
+console.log(dt.getDate());
 console.log(dt.getDay());
 console.log(dt.getHours());
 console.log(dt.getMinutes());
@@ -14,11 +14,29 @@ console.log(dt.getHours());
 
 console.log(Math.PI);
 
-Array=[8,6,7,9,8];
-console.log(Math.max(9,1));
-console.log(Math.round(9.48));
-console.log(Math.sqrt(9));
-console.log(Math.ceil(2.4));
-console.log(Math.floor(2.4));
-console.log(Math.abs(-232))
+Array = [8, 6, 7, 9, 8];
+// console.log(Math.max(9,1));
+// console.log(Math.round(9.48));
+// console.log(Math.sqrt(9));
+// console.log(Math.ceil(2.4));
+// console.log(Math.floor(2.4));
+// console.log(Math.abs(-232))
 
+Array.unshift(3, 4);
+console.log(Array);
+
+//Making a circle in js
+
+const inp = document.querySelector("#input");
+inp.addEventListener("input", circle);
+
+function circle() {
+  document.querySelector("#circle").style.height = getRadius() + "px";
+  document.querySelector("#circle").style.width = getRadius() + "px";
+}
+
+function getRadius() {
+  
+  radius = inp.value;
+  return radius;
+}
