3f39de1ba00f9a8528569e73827524ff9225da79
Shivproj <shivindrakanti@gmail.com>
2022-11-22 16:12:43 +0530

    nodemon exolained

--- Node JS/src/index.js
+++ Node JS/src/index.js
@@ -1,27 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const students = require("./sample2.json")
+
+
 
 const app = express();
 app.get("/",(req,res)=>{
     res.status(200).send("Hey")});
-app.get("/students",(req,res)=>{
-    const students=[{
-        
-            "Name": "Test",
-            "Mobile": 12345678,
-            "Boolean": true,
-            "Pets": [
-              "Dog",
-              "cat"
-            ],
-            "Address": {
-              "Permanent address": "USA",
-              "current Address": "AU"
-            }
-          
+app.get("/students",(req,res)=>{   
+            
 
-    }];
+    
     res.status(200).send(students)
     
 
